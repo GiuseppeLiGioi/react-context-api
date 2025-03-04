0ff3f7c41f776a78000fceb00a19837e588d1db8
GiuseppeLiGioi <pepe2117k@gmail.com>
2025-03-04 15:58:46 +0100

    add: API calls in the GlobalContext.

--- client/src/context/GlobalContext.jsx
+++ client/src/context/GlobalContext.jsx
@@ -3,4 +3,21 @@ import { createContext, useState, useContext } from "react";
 import axios from "axios";
 
 
-const GlobalProvider = createContext();
+const GlobalContext= createContext();
+
+const GlobalProvider = ( {children} ) => {
+    const [ posts, setPosts ]  = useState ([]);
+    const[singlePost, setSinglePost] = useState( {} );
+
+
+    const fetchPosts = () => {
+        axios.get('http://localhost:3000/api/posts').then( (res) => setPosts( res.data ) )
+
+    }
+
+    const fetchSinglePost = (id) => {
+        axios.get(`http://localhost:3000/api/posts/${id}`).then( (res) => setSinglePost( res.data ) )
+
+    }
+
+}
